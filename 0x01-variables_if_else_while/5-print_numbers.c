d8212c75021f9f1a003c1cc685b240380097d88f
wambj <judykibe20@gmail.com>
2023-02-20 12:09:47 -0800

    Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from zero
+ * with a base of ten
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
