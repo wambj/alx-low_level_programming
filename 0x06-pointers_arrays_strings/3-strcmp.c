b7bacc31a47d1bb785a10664b4c99c3ef66423bf
wambj <judykibe20@gmail.com>
2023-03-03 03:18:42 -0800

    Adding strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp -> for comparing purpose
+ * @s1: param1
+ * @s2: param2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2);
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
