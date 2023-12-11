ae56a7b3080c24f99b651b5eb4d89f0411c8bd37
Judy Kibe <judykibe20@gmail.com>
2023-12-11 09:06:30 -0800

    Palindrome value

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s)
+
+/**
+ * is_palindrome - checks if value is same forward as back
+ * @s: value of string
+ * Return: 1 if true 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+	{
+		return (1);
+	}
+	return (checkpal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: value of string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks char for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of string
+ * Return: 1 if true 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + 1) != *(s + len - 1))
+	{
+		return (0);
+	}
+	if (i >= len)
+	{
+		return (1);
+	}
+	return (check_pal(s, i + 1, len - 1));
+}
